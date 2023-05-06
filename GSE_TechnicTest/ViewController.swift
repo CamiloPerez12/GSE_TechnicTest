e41a79546ac9f9bbc1e8b00c1814a46ed3cef6ac
pedroVanegas <pedro.vanegas@farmatodo.com>
2023-05-06 08:53:18 -0500

    Initial Commit

---
+++ GSE_TechnicTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GSE_TechnicTest
+//
+//  Created by Pedro Vanegas on 6/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
